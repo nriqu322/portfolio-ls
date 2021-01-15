6f4305d6bcaf6822fd89533c3d9edf1b2ef89791
Luis Saavedra <nriqu322@gmail.com>
2021-01-15 12:59:58 -0600

    complete the grid template for portfolio section and style

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,23 +1,144 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import {
+  Container, Row, Card, Button,
+} from 'react-bootstrap';
 import defaultImage from '../assets/images/default-image.jpg';
 
 const Portfolio = () => (
   <div>
     <h1>My Recent Work</h1>
-    <div className="portfolio-cards">
-      <Card style={{ width: '369', height: '474px' }}>
-        <Card.Img variant="top" src={defaultImage} style={{ height: '272px' }} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card&apos;s content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <Container className="portfolio-cards">
+      <Row className="card-container-row">
+        <Card className="card-container" style={{ width: '320px', height: '411px' }}>
+          <Card.Img variant="top" src={defaultImage} style={{ height: '236px' }} />
+          <Card.Body>
+            <Card.Title className="card-title">Card Title</Card.Title>
+            <div className="d-flex card-stack-container">
+              <Card.Text className="card-stack">
+                Javascript
+              </Card.Text>
+              <Card.Text className="card-stack">
+                React
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Bootstrap
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Netlify
+              </Card.Text>
+            </div>
+            <Button className="mt-2" href="#" variant="success">See Details</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card-container" style={{ width: '320px', height: '411px' }}>
+          <Card.Img variant="top" src={defaultImage} style={{ height: '236px' }} />
+          <Card.Body>
+            <Card.Title className="card-title">Card Title</Card.Title>
+            <div className="d-flex card-stack-container">
+              <Card.Text className="card-stack">
+                Javascript
+              </Card.Text>
+              <Card.Text className="card-stack">
+                React
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Bootstrap
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Netlify
+              </Card.Text>
+            </div>
+            <Button className="mt-2" href="#" variant="success">See Details</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card-container" style={{ width: '320px', height: '411px' }}>
+          <Card.Img variant="top" src={defaultImage} style={{ height: '236px' }} />
+          <Card.Body>
+            <Card.Title className="card-title">Card Title</Card.Title>
+            <div className="d-flex card-stack-container">
+              <Card.Text className="card-stack">
+                Javascript
+              </Card.Text>
+              <Card.Text className="card-stack">
+                React
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Bootstrap
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Netlify
+              </Card.Text>
+            </div>
+            <Button className="mt-2" href="#" variant="success">See Details</Button>
+          </Card.Body>
+        </Card>
+        {/* </Row> */}
+        {/* <Row> */}
+        <Card className="card-container" style={{ width: '320px', height: '411px' }}>
+          <Card.Img variant="top" src={defaultImage} style={{ height: '236px' }} />
+          <Card.Body>
+            <Card.Title className="card-title">Card Title</Card.Title>
+            <div className="d-flex card-stack-container">
+              <Card.Text className="card-stack">
+                Javascript
+              </Card.Text>
+              <Card.Text className="card-stack">
+                React
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Bootstrap
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Netlify
+              </Card.Text>
+            </div>
+            <Button className="mt-2" href="#" variant="success">See Details</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card-container" style={{ width: '320px', height: '411px' }}>
+          <Card.Img variant="top" src={defaultImage} style={{ height: '236px' }} />
+          <Card.Body>
+            <Card.Title className="card-title">Card Title</Card.Title>
+            <div className="d-flex card-stack-container">
+              <Card.Text className="card-stack">
+                Javascript
+              </Card.Text>
+              <Card.Text className="card-stack">
+                React
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Bootstrap
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Netlify
+              </Card.Text>
+            </div>
+            <Button className="mt-2" href="#" variant="success">See Details</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card-container" style={{ width: '320px', height: '411px' }}>
+          <Card.Img variant="top" src={defaultImage} style={{ height: '236px' }} />
+          <Card.Body>
+            <Card.Title className="card-title">Card Title</Card.Title>
+            <div className="d-flex card-stack-container">
+              <Card.Text className="card-stack">
+                Javascript
+              </Card.Text>
+              <Card.Text className="card-stack">
+                React
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Bootstrap
+              </Card.Text>
+              <Card.Text className="card-stack">
+                Netlify
+              </Card.Text>
+            </div>
+            <Button className="mt-2" href="#" variant="success">See Details</Button>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
   </div>
 );
 
